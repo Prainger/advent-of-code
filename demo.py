861e9d034b6fb8809aa6fac33cc02e7e95e30947
Alexandr Vyroubal <xvyroub@fi.muni.cz>
2018-12-06 17:58:41 +0100

    day05

---
+++ demo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from day05.day05 import react_polymer, remove_unit_and_react
+
+
+def main():
+
+    '''
+    # day05
+    react_polymer('input.txt')
+    remove_unit_and_react('input.txt')
+    '''
+    return
+main()
